a60987ce7b0a35e6ec66dc7e6cf21eaa9c335a2d
Narcob47 <137599138+Narcob47@users.noreply.github.com>
2024-12-27 03:53:25 +0200

    cors

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-gasxq&(!k31)i#v)t03uudzbv@+m@xdkekkt1*-dy29qosu1(#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'http://localhost:3000', 'http://127.0.0.1:3000', '']
+ALLOWED_HOSTS = ['*', 'http://localhost:3000', 'http://127.0.0.1:3000', 'wmszm.org']
 
 
 # Application definition
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "wmszm.org",
 ]
 
 ROOT_URLCONF = 'core.urls'
